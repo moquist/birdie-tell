16a031e787f816335188d6e602609c3d3052ca14
Matt Oquist <moquist@majen.net>
2017-08-23 16:51:27 -0400

    [scamp] pull in numeric-tower dep

--- modules/scamp/project.clj
+++ modules/scamp/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [com.taoensso/timbre "4.3.1"]
-                 [prismatic/schema "1.1.1"]])
+                 [prismatic/schema "1.1.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]])
