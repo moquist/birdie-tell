7c8ad0a5951f5d6bc75958614a24f5800533389a
Matt Oquist <moquist@majen.net>
2017-08-23 16:57:07 -0400

    [scamp/world] move all world-aware schemas and fns to separate ns

---
+++ modules/scamp/src/world/main.clj
@@ -0,0 +1,114 @@
+(ns world.main
+  (:require [clojure.string :as str]
+            [schema.core :as s]
+            [scamp.core :as scamp]
+            [scamp.util :as util]))
+
+(def WorldSchema
+  {:message-envelopes [scamp/MessageEnvelopeSchema]
+   :config scamp/ClusterConfigSchema
+   :network {scamp/NodeContactAddressSchema scamp/NetworkedNodeSchema}})
+
+(def world-base
+  "Return a new, pristine world."
+  {:message-envelopes []
+   :config scamp/default-config
+   :network {}})
+
+(s/defn world-add-new-node :- WorldSchema
+  "Update world to 'turn on' node."
+  [world :- WorldSchema
+   networked-node :- scamp/NetworkedNodeSchema]
+  (assoc-in world [:network (scamp/node->node-contact-address (:self networked-node))]
+            networked-node))
+
+(s/defn world-add-messages :- WorldSchema
+  "Given 'world and new messages to add, return 'world with the new
+  messages added."
+  [world :- WorldSchema
+   new-messages :- [scamp/MessageEnvelopeSchema]]
+  (update-in world [:message-envelopes] util/concatv new-messages))
+
+(s/defn world-subscribe-new-node :- WorldSchema
+  "Given 'world, a 'new-node-contact-address for the node that is
+  subscribing, and a 'node, add 'new-node-contact-address to 'world
+  and inject a :new-subscription message into 'world.
+
+  Return 'world with the new node and a :new-subscription message.
+
+  [1] 2.2.1 Subscription: Algorithm 1"
+  [world :- WorldSchema
+   new-node-contact-address :- scamp/NodeContactAddressSchema
+   contact-node-address :- scamp/NodeContactAddressSchema]
+  (let [new-node (scamp/init-new-subscriber (:config world)
+                                           new-node-contact-address
+                                           contact-node-address)
+        new-subscription-message (scamp/msg->envelope contact-node-address
+                                                     :new-subscription
+                                                     new-node-contact-address)]
+    (-> world
+        (world-add-new-node new-node)
+        (world-add-messages [new-subscription-message]))))
+
+(s/defn world-instruct-node-to-unsubscribe :- WorldSchema
+  "This is a mechanism to make unsubscription happen in this toy
+  implementation by inserting a message into 'world from outside the
+  'world.
+
+  Given 'world and the id of a node that is unsubscribing, add a
+  :node-unsubscription message to and return 'world."
+  [world :- WorldSchema
+   node-to-unsubscribe :- scamp/NodeContactAddressSchema]
+  (world-add-messages world [(scamp/msg->envelope node-to-unsubscribe
+                                                 :node-unsubscription
+                                                 node-to-unsubscribe)]))
+
+(s/defn world-do-async-processing :- WorldSchema
+  [world :- WorldSchema]
+  (let [comm-updates (mapcat (partial scamp/node-do-async-processing (:config world)) (-> world :network vals))]
+    #_(println :t (*milli-time*) :comm-updates comm-updates)
+    world))
+
+(s/defn world-do-comm :- WorldSchema
+  "Take 'world. Process one message. Return new 'world."
+  [{:keys [config] :as world} :- WorldSchema
+   & [options :- {s/Keyword s/Any}]]
+  (if (-> world :message-envelopes empty?)
+    world
+    (let [[[_ destination-node-id & message] & message-envelopes] (:message-envelopes world)
+          destination-node (get-in world [:network destination-node-id])
+          [new-destination-node new-message-envelopes] (scamp/read-mail config
+                                                                        destination-node
+                                                                        message)]
+      (when (:verbose? options)
+        (println "message to " destination-node-id ": " message))
+      (scamp/tick-clock-millis!) ; This makes 1000 msg/"second" the upper bound of throughput.
+      (world-do-async-processing world)
+      (-> world
+          (assoc :message-envelopes (util/concatv message-envelopes new-message-envelopes))
+          (assoc-in [:network destination-node-id] new-destination-node)))))
+
+(s/defn world-do-all-comms :- WorldSchema
+  "Take 'world. Process communication until no unread messages remain.
+   Return world."
+  ([world :- WorldSchema] (world-do-all-comms world {}))
+  ([world :- WorldSchema
+    options :- {s/Keyword s/Any}]
+   (if (-> world :message-envelopes empty?)
+     world
+     (recur (world-do-comm world options) options))))
+
+(s/defn world->dot
+  [world :- WorldSchema]
+  (let [node-id->int #(str/replace % #"node-id" "")]
+    (str "digraph {"
+         (->> (:network world)
+              (reduce (fn [r [self {:keys [downstream]}]]
+                        (apply str r (map #(str (node-id->int self)
+                                                " -> "
+                                                (node-id->int %)
+                                                ";")
+                                          downstream)))
+                      ""))
+         "}")))
+
---
+++ modules/scamp/test/world/main_test.clj
@@ -0,0 +1,194 @@
+(ns world.main-test
+  (:require [clojure.test :refer :all]
+            [schema.core :as s]
+            [world.main :as main]
+            [scamp.core :as scamp]
+            [scamp.core-test :as scamp-test]))
+
+(def random-instance (atom (java.util.Random.)))
+
+(defn reset-rand-state! [& seed]
+  (let [seed (or seed 43)]
+    (.setSeed @random-instance seed)))
+
+(defn- testing-rand*
+  ([] (.nextFloat @random-instance))
+  ([n] (* n (testing-rand*))))
+
+(defn- purge-envelope-id [envelope]
+  (vec (butlast envelope)))
+
+(defn- purge-world-envelope-ids [world]
+  (update world :message-envelopes #(mapv purge-envelope-id %)))
+
+(defn- purge-world-async-state [world]
+  (reduce (fn [world [node-contact-address networked-node]]
+            (assoc-in world [:network node-contact-address]
+                      (assoc networked-node
+                             :send-next-heartbeats-milli-time nil
+                             :heartbeat-timeout-milli-time nil)))
+          world
+          (:network world)))
+
+(deftest world-subscribe-new-node-test
+  (scamp-test/scamp-test
+   #(is (= (-> main/world-base
+               (main/world-add-new-node (scamp/node-contact-address->node "node-id0"))
+               (main/world-subscribe-new-node "node-id1" "node-id0")
+               (dissoc :config)
+               purge-world-envelope-ids
+               purge-world-async-state)
+           {:message-envelopes
+            [[:message-envelope "node-id0" :new-subscription "node-id1"]],
+            :network
+            {"node-id0"
+             (assoc (scamp/node-contact-address->node "node-id0")
+                    :upstream #{},
+                    :downstream #{},
+                    :messages-seen {}),
+             "node-id1"
+             (assoc (scamp/node-contact-address->node "node-id1")
+                    :upstream #{},
+                    :downstream #{"node-id0"},
+                    :messages-seen {})}}))))
+
+
+(defn world-with-subs [subscriptions-count]
+  (let [node-name #(str "node-id" %)
+        world (main/world-add-new-node main/world-base
+                                       (scamp/node-contact-address->node (node-name 0)))]
+    (loop [world world
+           n 0]
+      (if (>= n subscriptions-count)
+        world
+        (let [new-node-id-num (inc n)]
+          (recur (main/world-do-all-comms
+                  (main/world-subscribe-new-node world
+                                                 (node-name new-node-id-num)
+                                                 (node-name n)))
+                 new-node-id-num))))))
+
+(deftest do-comm-test
+  (scamp-test/scamp-test
+   #(let [world (world-with-subs 3)
+          end-world (-> (reduce
+                         (fn [world _n] (main/world-do-comm world))
+                         world
+                         (range 9))
+                        (dissoc :config)
+                        purge-world-envelope-ids)]
+      (is (= (purge-world-async-state end-world)
+             {:message-envelopes [],
+              :network
+              {"node-id0"
+               (assoc (scamp/node-contact-address->node "node-id0")
+                      :upstream #{"node-id1"},
+                      :downstream #{"node-id2" "node-id3"},
+                      :messages-seen {"1" 1, "3" 1, "4" 1, "5" 5, "9" 1}),
+               "node-id1"
+               (assoc (scamp/node-contact-address->node "node-id1")
+                      :upstream #{"node-id2"},
+                      :downstream #{"node-id2" "node-id3" "node-id0"},
+                      :messages-seen {"2" 1, "4" 1, "5" 9, "9" 2, "10" 1, "11" 5}),
+               "node-id2"
+               (assoc (scamp/node-contact-address->node "node-id2")
+                      :upstream #{"node-id3" "node-id0" "node-id1"},
+                      :downstream #{"node-id3" "node-id1"},
+                      :messages-seen
+                      {"6" 1, "4" 1, "5" 10, "7" 1, "8" 1, "9" 1, "11" 5}),
+               "node-id3"
+               (assoc (scamp/node-contact-address->node "node-id3")
+                      :upstream #{"node-id2" "node-id0" "node-id1"},
+                      :downstream #{"node-id2"},
+                      :messages-seen {"12" 1, "13" 1, "11" 3, "14" 1})}}
+             )))))
+
+(s/defn do-comms :- main/WorldSchema
+  "Take 'world and 'n. Process up to 'n messages. Return new 'world.
+   If 'world has no :message-envelopes before 'n messages have been
+   processed, 'world is returned immediately."
+  [world :- main/WorldSchema
+   n :- s/Int]
+  (if (or (zero? n)
+          (empty? (:message-envelopes world)))
+    world
+    (recur (main/world-do-comm world) (dec n))))
+
+(deftest receive-msg-node-unsubscription-test
+  (scamp-test/scamp-test
+   #(let [world (-> (world-with-subs 43)
+                    main/world-do-all-comms
+                    (main/world-instruct-node-to-unsubscribe "node-id19")
+                    main/world-do-comm)]
+      (clojure.pprint/pprint (:message-envelopes world))
+      (is (= (:message-envelopes world)
+             [[:message-envelope
+               "node-id16"
+               :node-replacement
+               {:old "node-id19", :new "node-id21"}
+               "286"]
+              [:message-envelope
+               "node-id21"
+               :node-replacement
+               {:old "node-id19", :new "node-id16"}
+               "287"]
+              [:message-envelope "node-id17" :node-removal "node-id19" "288"]
+              [:message-envelope "node-id20" :node-removal "node-id19" "289"]
+              [:message-envelope "node-id18" :node-removal "node-id19" "290"]
+              [:message-envelope "node-id23" :node-removal "node-id19" "291"]]
+             )))))
+
+(defn anticipated-scamp-arcs
+  "Given 'connection-redundancy and 'n nodes, how many arcs is the
+  graph anticipated to have?"
+  [connection-redundancy n]
+  (* (inc connection-redundancy)
+     n
+     ;; TODO: which log is this supposed to be?
+     (Math/log n)))
+
+(defn demo-unsubscription [output-filename-base]
+  (scamp-test/scamp-test
+   #(let [world (-> main/world-base
+                    (assoc-in [:config :connection-redundancy] -1)
+                    (main/world-add-new-node (assoc (scamp/node-contact-address->node "node-id0")
+                                                    :upstream #{"node-id2"}
+                                                    :downstream #{"node-id1"}))
+                    (main/world-add-new-node (assoc (scamp/node-contact-address->node "node-id1")
+                                                    :upstream #{"node-id0"}
+                                                    :downstream #{"node-id3" "node-id4"}))
+                    (main/world-add-new-node (assoc (scamp/node-contact-address->node "node-id2")
+                                                    :upstream #{"node-id4"}
+                                                    :downstream #{"node-id0"}))
+                    (main/world-add-new-node (assoc (scamp/node-contact-address->node "node-id3")
+                                                    :upstream #{"node-id1"}
+                                                    :downstream #{"node-id4"}))
+                    (main/world-add-new-node (assoc (scamp/node-contact-address->node "node-id4")
+                                                    :upstream #{"node-id1" "node-id3"}
+                                                    :downstream #{"node-id2"})))
+          world-2 (-> world
+                      (main/world-instruct-node-to-unsubscribe "node-id4")
+                      (main/world-do-all-comms {:verbose? true}))
+          ]
+      (spit (str output-filename-base "-01.dot") (main/world->dot world))
+      (spit (str output-filename-base "-02.dot") (main/world->dot world-2))
+      )))
+
+(defn pprint-cluster [world]
+  (->> world
+       :network
+       (map (fn [[k v]] [k (select-keys v [:upstream :downstream])]))
+       (into {})
+       clojure.pprint/pprint))
+
+(comment
+  (def mesh-size 100)
+  (spit "/tmp/mesh.dot" (str "digraph {"
+                             (apply str
+                                    (apply concat
+                                           (filter (complement nil?)
+                                                   (for [s (range mesh-size)]
+                                                     (for [d (range mesh-size)]
+                                                       (when (not= s d)
+                                                         (str s "->" d ";"))))))) "}"))
+  )
